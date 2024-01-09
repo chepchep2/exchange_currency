ef77d3ea4e07957552fafce8ad106fc8ea77b6f8
rickyAHNN <smrmaster88@gmail.com>
2024-01-09 16:18:23 +0900

    commit3

--- lib/ui/main/main_screen.dart
+++ lib/ui/main/main_screen.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
 
-class ExchangeCurrencyMainScreen extends StatelessWidget {
+class ExchangeCurrencyMainScreen extends StatefulWidget {
   const ExchangeCurrencyMainScreen({super.key});
 
   @override
+  State<ExchangeCurrencyMainScreen> createState() => _ExchangeCurrencyMainScreenState();
+}
+
+class _ExchangeCurrencyMainScreenState extends State<ExchangeCurrencyMainScreen> {
+
+  final dropDown = [];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-    ),
+      appBar: AppBar(title: const Text('환율 계산기'),),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(keyboardType:TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: '기준통화금액'
+              ),
+              onTap: (){},),
+          DropdownButton(items: const [
+              DropdownMenuItem(child: Text('')),
+              DropdownMenuItem(child: Text('')),
+              DropdownMenuItem(child: Text('')),
+          ], onChanged: (value){},),
+        ],
+            ),
+      ),
     );}}
