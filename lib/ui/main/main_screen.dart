04d24f7514edd5e2662746c309f2da9eb4f384d9
sangwoo <kajer102@gmail.com>
2024-01-09 23:12:14 +0900

    feat: 여러 나라가 나옴 연지민님께서 도와주심

--- lib/ui/main/main_screen.dart
+++ lib/ui/main/main_screen.dart
@@ -23,6 +23,25 @@ class _ExchangeCurrencyMainScreenState
   // 환율이 적용되는 통화
   final repository = RateRepositoryImpl();
 
+  List<String> currencyCodes = [
+    'AED', 'AFN', 'ALL', 'AMD', 'ANG', 'AOA', 'ARS', 'AUD', 'AWG', 'AZN',
+    'BAM', 'BBD', 'BDT', 'BGN', 'BHD', 'BIF', 'BMD', 'BND', 'BOB', 'BRL',
+    'BSD', 'BTN', 'BWP', 'BYN', 'BZD', 'CAD', 'CDF', 'CHF', 'CLP', 'CNY',
+    'COP', 'CRC', 'CUP', 'CVE', 'CZK', 'DJF', 'DKK', 'DOP', 'DZD', 'EGP',
+    'ERN', 'ETB', 'EUR', 'FJD', 'FKP', 'FOK', 'GBP', 'GEL', 'GGP', 'GHS',
+    'GIP', 'GMD', 'GNF', 'GTQ', 'GYD', 'HKD', 'HNL', 'HRK', 'HTG', 'HUF',
+    'IDR', 'ILS', 'IMP', 'INR', 'IQD', 'IRR', 'ISK', 'JEP', 'JMD', 'JOD',
+    'JPY', 'KES', 'KGS', 'KHR', 'KID', 'KMF', 'KRW', 'KWD', 'KYD', 'KZT',
+    'LAK', 'LBP', 'LKR', 'LRD', 'LSL', 'LYD', 'MAD', 'MDL', 'MGA', 'MKD',
+    'MMK', 'MNT', 'MOP', 'MRU', 'MUR', 'MVR', 'MWK', 'MXN', 'MYR', 'MZN',
+    'NAD', 'NGN', 'NIO', 'NOK', 'NPR', 'NZD', 'OMR', 'PAB', 'PEN', 'PGK',
+    'PHP', 'PKR', 'PLN', 'PYG', 'QAR', 'RON', 'RSD', 'RUB', 'RWF', 'SAR',
+    'SBD', 'SCR', 'SDG', 'SEK', 'SGD', 'SHP', 'SLE', 'SOS', 'SRD', 'SSP',
+    'STN', 'SYP', 'SZL', 'THB', 'TJS', 'TMT', 'TND', 'TOP', 'TRY', 'TTD',
+    'TVD', 'TWD', 'TZS', 'UAH', 'UGX', 'USD', 'UYU', 'UZS', 'VES', 'VND',
+    'VUV', 'WST', 'XAF', 'XCD', 'XDR', 'XOF', 'XPF', 'YER', 'ZAR', 'ZMW', 'ZWL'
+  ];
+
   @override
   void dispose() {
     textController.dispose();
@@ -69,10 +88,11 @@ class _ExchangeCurrencyMainScreenState
                 children: [
                   DropdownButton(
                     value: selectCurrency,
-                    items: const [
-                      DropdownMenuItem(value: 'KRW', child: Text('KRW')),
-                      DropdownMenuItem(value: 'USD', child: Text('USD')),
-                    ],
+                    // items: const [
+                    //   DropdownMenuItem(value: 'KRW', child: Text('KRW')),
+                    //   DropdownMenuItem(value: 'USD', child: Text('USD')),
+                    // ],
+                    items: currencyCodes.map((e) => DropdownMenuItem(child: Text(e), value: e,)).toList(),
                     onChanged: (value) {
                       setState(() {
                         selectCurrency = value!;
