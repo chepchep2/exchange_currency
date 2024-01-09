388d9068016f4b6a8858568de4f7fd8a77b8cc0e
rickyAHNN <smrmaster88@gmail.com>
2024-01-09 16:02:00 +0900

    commit2

--- lib/ui/main/main_screen.dart
+++ lib/ui/main/main_screen.dart
@@ -1,26 +1,11 @@
 import 'package:flutter/material.dart';
 
-<<<<<<< HEAD
-class ExchageCurrencyMainScreen extends StatelessWidget {
-  const ExchageCurrencyMainScreen({super.key});
+class ExchangeCurrencyMainScreen extends StatelessWidget {
+  const ExchangeCurrencyMainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ,
-    )
-
-=======
-class MainScreen extends StatelessWidget {
-  const MainScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('1ㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇ'),
-      ),
-    );
->>>>>>> origin/main
-  }
-}
+      body: Column(
+    ),
+    );}}
